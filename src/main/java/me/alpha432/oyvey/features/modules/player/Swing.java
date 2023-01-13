a99c4fa3f7c4b17d0181fb56ea4d188d6babd80c
browsky300 <browsky300@gmail.com>
2023-01-13 09:35:49 -0800

    friday 13th update crazy and bonkers

--- src/main/java/me/alpha432/oyvey/features/modules/player/Swing.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/Swing.java
@@ -8,6 +8,7 @@ public class Swing extends Module {
     
     public enum SwingMode {Mainhand, Offhand, Offhandc, None, Default}
     public Setting<SwingMode> swingmode = register(new Setting("Mode", SwingMode.Mainhand));
+    public Setting<Float> swingspeed = register(new Setting("Speed", Float.valueOf(1.0f), Float.valueOf(0.0f), Float.valueOf(1.0f)));
     
     public Swing() {
         super("Swing", "options for swinging", Module.Category.PLAYER, false, false, false);
---
+++ src/main/java/me/alpha432/oyvey/mixin/mixins/MixinEntityLivingBase.java
@@ -0,0 +1,18 @@
+package me.alpha432.oyvey.mixin.mixins;
+
+import me.alpha432.oyvey.features.modules.player.Swing;
+import me.alpha432.oyvey.features.command.Command;
+import net.minecraft.entity.EntityLivingBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityLivingBase.class)
+public class MixinEntityLivingBase {
+    @Inject(method = "getArmSwingAnimationEnd",at = @At(value = "HEAD"), cancellable = true)
+    public void getArmSwingAnimationEndHook(CallbackInfoReturnable<Integer> info) {
+        int i = Swing.getINSTANCE().isOn() ? (int) Math.floor(1 / Swing.getINSTANCE().swingspeed.getValue() * 6) : 6;
+        info.setReturnValue(i);
+    }
+}
