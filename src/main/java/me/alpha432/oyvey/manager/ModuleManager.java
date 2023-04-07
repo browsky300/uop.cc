069a62668a281e50c62eac50f02bc346f55d8d50
browsky300 <browsky300@gmail.com>
2023-04-06 20:30:57 -0700

    ?

--- src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
+++ src/main/java/me/alpha432/oyvey/manager/ModuleManager.java
@@ -64,7 +64,6 @@ public class ModuleManager
         this.modules.add(new CustomTime());
         this.modules.add(new ExtraTab());
         this.modules.add(new FakeDuelMessage());
-        this.modules.add(new FutureColor());
         this.modules.add(new GoodLuckCharm());
         this.modules.add(new PacketLogger());
         this.modules.add(new PopCounter());
